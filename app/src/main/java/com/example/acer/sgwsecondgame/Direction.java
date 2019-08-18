485a2d8c4db5801f996536ad6c4c28fa1237909e
sgw <2357776435@qq.com>
2019-08-18 11:36:17 +0800

    2048游戏

---
+++ app/src/main/java/com/example/acer/sgwsecondgame/Direction.java
@@ -0,0 +1,10 @@
+package com.example.acer.sgwsecondgame;
+
+public interface Direction {
+    //1：向上，2：向下，3：向左，4：向右,0：未滑动
+    static final int LEFT=3;
+    static final int RIGHT=4;
+    static final int UP=1;
+    static final int DOWN=2;
+    static final int NONE=0;
+}
